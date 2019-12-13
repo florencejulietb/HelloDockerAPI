f7c5f6727e954bc001bcb998ccee0dd01c278f09
Florence Juliet B <florence.juliet.b@accenture.com>
2019-12-13 11:08:04 +0530

    Initial code

---
+++ HelloDockerAPI/HelloDockerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloDockerAPI
+{
+    public class HelloDockerService
+    {
+        public string SayHello(string inputMsg)
+        {
+            return "Welcome " + inputMsg;
+        }
+    }
+}
